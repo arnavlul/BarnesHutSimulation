9dcaeddea500dd67c655e7e83e1cebb4ce695724
Arnav <arnavtyagi042@gmail.com>
2026-05-28 06:48:31 +0530

    Add project files.

---
+++ BarnesHutSimulation.cpp
@@ -0,0 +1,20 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <glm/glm.hpp>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    if (!glfwInit()) {
+        cerr << "Failed to load GLFW" << endl;
+        return -1;
+    }
+
+    glfwWindowHint
+
+    return 0;
+}
